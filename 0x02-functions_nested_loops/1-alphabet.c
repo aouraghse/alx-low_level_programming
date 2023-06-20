d457fc4a98a36e39e91ce7fb2df556c5f9410f72
aouraghse <aouraghse@gmail.com>
2023-06-20 16:32:00 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+	/**
+	 * print_alphabet - print all alphabet in lowercase
+	 */
+
+
+	void print_alphabet(void)
+	{
+		char alphabet;
+
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+			{
+			_putchar(alphabet);
+			}
+
+
+		_putchar('\n');
+}
